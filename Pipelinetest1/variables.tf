7eb77c38ab38a4fae0cfa52a831ad094de43ef68
JohnMorgan1234 <jcarlosgl96@protonmail.com>
2024-06-13 22:15:43 +0200

    added new tf to create ansible

---
+++ Pipelinetest1/variables.tf
@@ -0,0 +1,18 @@
+variable "region" {
+default = "us-east-1a"
+}
+variable "instance_type" {
+default = "t2.micro"
+}
+variable "profile_name" {
+default = "default"
+}
+variable "instance_key" {
+default = "test"
+}
+variable "vpc_cidr" {
+default = "178.0.0.0/16"
+}
+variable "public_subnet_cidr" {
+default = "178.0.10.0/24"
+}
